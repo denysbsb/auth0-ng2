149e3cff386e9a6f48e4e1cc8811bc314f5fc607
denysbsb <denys.design@gmail.com>
2016-10-23 18:53:20 -0200

    app module imports components

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {AUTH_PROVIDERS} from 'angular2-jwt';
+
+import {HomeComponent} from './components/home/home.component';
+import {PerfilComponent} from './components/perfil/perfil.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,HomeComponent,PerfilComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
