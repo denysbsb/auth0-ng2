7ad4182efb99226a81453942b4fe1c0406e49a25
denysbsb <denys.design@gmail.com>
2016-10-23 18:42:22 -0200

    components start

---
+++ app/components/perfil/perfil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'perfil',
+    templateUrl: 'perfil.component.html'
+})
+export class PerfilComponent { 
+
+}
