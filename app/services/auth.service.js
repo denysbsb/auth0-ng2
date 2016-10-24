616a31c328f78176a829dfc1b2af8698c391a422
denysbsb <denys.design@gmail.com>
2016-10-24 09:41:39 -0200

    hide options

--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -10,12 +10,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var angular2_jwt_1 = require('angular2-jwt');
-var auth_options_1 = require('../auth.options');
 var Auth = (function () {
     function Auth() {
         var _this = this;
         // Configure Auth0
-        this.lock = new Auth0Lock('AAY6ZlfTHc32nHChNuvTFIX1klghYKsH', 'denys.auth0.com', auth_options_1.options);
+        this.lock = new Auth0Lock('AAY6ZlfTHc32nHChNuvTFIX1klghYKsH', 'denys.auth0.com', {});
         // Add callback for lock `authenticated` event
         this.lock.on("authenticated", function (authResult) {
             _this.lock.getProfile(authResult.idToken, function (error, profile) {
